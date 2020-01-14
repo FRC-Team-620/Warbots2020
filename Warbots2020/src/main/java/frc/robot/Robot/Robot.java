71af974eb87f3c8499fee39ed92a0728c5ebc137
Kieran <kierank.pc@gmail.com>
2020-01-13 19:13:43 -0500

    progress

--- Warbots2020/src/main/java/frc/robot/Robot/Robot.java
+++ Warbots2020/src/main/java/frc/robot/Robot/Robot.java
@@ -15,7 +15,7 @@ import frc.robot.DriveTrain.DriveTrain;
 import frc.robot.DriveTrain.DriveWithJoysticks;
 import frc.robot.DriveTrain.SitStill;
 
-public class Robot extends TimedRobot //Kieran was here
+public class Robot extends TimedRobot //Andrew was here
 {
   //region Overrides
   @Override
